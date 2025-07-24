62d54db7a29bb90d7bbc97342c438d89e2e7e872
Mohammed Al-Hashimi <malhash2@uwo.ca>
2025-07-24 19:39:36 -0400

    added delete key recognition

--- Kilo.c
+++ Kilo.c
@@ -22,7 +22,8 @@ enum editorKey {
     PAGE_UP,
     PAGE_DOWN,
     HOME_KEY,
-    END_KEY
+    END_KEY,
+    DEL_KEY
 };
 
 enum charEditorKey {
@@ -92,6 +93,7 @@ int editorReadKey() {
 		if (seq[2] == '~'){
 		    switch (seq[1]) {
 			case '1': return HOME_KEY;
+			case '3': return DEL_KEY;
 			case '4': return END_KEY;
 			case '5': return PAGE_UP; 
 			case '6': return PAGE_DOWN;
